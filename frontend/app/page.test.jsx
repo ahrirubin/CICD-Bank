87038d7ddc786fe3243f72f8167615f9b18165c9
ahrirubin <ahri.rubin@gmail.com>
2024-06-16 03:20:17 +0200

    added tests third time

--- frontend/app/page.test.jsx
+++ frontend/app/page.test.jsx
@@ -2,12 +2,12 @@
  * @jest-environment jsdom
  */
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
 import Page from "./page";
 
 it("App Router: Works with Client Components (React State)", () => {
   render(<Page />);
 
+  screen.debug();
   const headings = screen.queryAllByRole("heading");
   expect(headings).toHaveLength(2);
   expect(headings[0]).toHaveTextContent("Ahri Banken");
